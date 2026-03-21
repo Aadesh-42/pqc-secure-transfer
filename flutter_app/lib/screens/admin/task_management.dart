051c2c10b797558116c1cfada7ceb1153ebea82a
Aadesh-42 <aadi.angane07@gmail.com>
2026-03-21 14:55:02 +0530

    fix: robust task creation with granular diagnostics

--- flutter_app/lib/screens/admin/task_management.dart
+++ flutter_app/lib/screens/admin/task_management.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 import '../../services/api_service.dart';
 import '../../models/task.dart';
 
